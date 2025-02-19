c6f1b2d427358a6c31f04568308f03e36e4dfdf6
sepva <seppe.vanswegenoven@skynet.be>
2025-02-19 16:25:26 +0100

    added error handling and logging

--- server/index.js
+++ server/index.js
@@ -5,6 +5,20 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import { SurveyModel, SurveyResultModel } from './model/survey.js';
+import { createLogger, transports } from "winston"
+
+const logger = createLogger({
+    transports: [
+        new transports.File({ filename: 'error.log', level: 'error' }),
+        new transports.File({ filename: 'info.log', level: 'info' }),
+    ],
+    rejectionHandlers: [
+        new transports.File({ filename: 'rejections.log' })
+    ],
+    exceptionHandlers: [
+        new transports.File({ filename: 'exceptions.log' })
+    ]
+});
 
 dotenv.config()
 app.use(cors())
@@ -14,13 +28,21 @@ app.use(bodyParser.json())
 
 //check if server is running
 app.listen(8080, () => {
-    console.log('server listening on port 8080')
+    logger.info("Server started and listening")
 })
 
 //connect to mongodb
 mongoose.connect(process.env.MONGODB).then(() => {
-    console.log('connected to mongodb')
-});
+    logger.info("Server connected to mongodb")
+}).catch(e => logger.error(e));
+
+mongoose.connection.on('disconnected', () => logger.info('MongoDB disconnected'));
+mongoose.connection.on('reconnected', () => logger.info('MongoDB reconnected'));
+
+function handle_error(err, next) {
+    logger.error(err)
+    next(err)
+}
 
 async function get_survey() {
     const survey = await SurveyModel.aggregate().sample(1)
@@ -28,16 +50,19 @@ async function get_survey() {
 }
 
 //get survey
-app.get('/survey', (req, res) => {
-    get_survey().then(json =>
+app.get('/survey', (req, res, next) => {
+    get_survey().then(json => {
         res.json(json)
-    )
+        logger.info("Send survey!")
+    })
+        .catch((err) => handle_error(err, next))
 })
 
 //post results of survey
-app.post('/survey', (req, res) => {
+app.post('/survey', (req, res, next) => {
     SurveyResultModel.create({
         email: req.body.email,
         resultJson: JSON.stringify(req.body.result)
-    });
+    }).then(() => logger.info("Survey saved in db!"))
+        .catch((err) => handle_error(err, next));
 })
